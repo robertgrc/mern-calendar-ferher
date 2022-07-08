7ea9afb4e52ba257c35da5bc8d4f49895c57e099
robertgrc <robert.rodriguez.rgrc@gmail.com>
2022-07-08 18:07:05 -0400

    351. Obtener la información del formulario del evento

--- src/calendar/components/CalendarModal.jsx
+++ src/calendar/components/CalendarModal.jsx
@@ -1,4 +1,4 @@
-import { addHours } from 'date-fns';
+import { addHours, differenceInSeconds } from 'date-fns';
 import { useState } from 'react';
 import DatePicker, {registerLocale} from "react-datepicker";
 import Modal from 'react-modal';
@@ -52,6 +52,21 @@ export const CalendarModal = () => {
         setIsOpen(false)
     }
 
+    const onSubmit = () => {
+        event.preventDefault()
+
+        const difference = differenceInSeconds( formValues.end, formValues.start);
+        
+        if( isNaN( difference)|| difference<=0){
+            console.log('Error en las Fechas')
+            return;
+        }
+
+        if (formValues.title.length<=0) return;
+
+        console.log(formValues);
+    }
+
   return (
     <Modal
         isOpen={isOpen }
@@ -63,7 +78,7 @@ export const CalendarModal = () => {
     >
        <h1> Nuevo evento </h1>
             <hr />
-            <form className="container">
+            <form className="container" onSubmit={onSubmit}>
 
                 <div className="form-group mb-2">
                     <label>Fecha y hora inicio</label>
