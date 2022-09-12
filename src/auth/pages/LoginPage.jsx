de90460aa1186e609e9e4157c262dcb0643ad212
robertgrc <robert.rodriguez.rgrc@gmail.com>
2022-09-12 12:09:37 -0400

    408 mostrar error en la autenticacion

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -4,6 +4,8 @@ import React from 'react';
 import { useForm } from '../../hooks/useForm';
 import { useAuthStore } from '../../hooks';
 import './LoginPage.css';
+import { useEffect } from 'react';
+import Swal from 'sweetalert2';
 
 const loginFormFields = {
     loginEmail: '',
@@ -22,7 +24,7 @@ const registerFormFields = {
 export const LoginPage = () => {
 
 
-    const { startLogin } = useAuthStore();
+    const { startLogin, errorMessage } = useAuthStore();
 
     const { loginEmail, loginPassword, onInputChange: onLoginInputChange } = useForm( loginFormFields );
     const { registerName, registerEmail, registerPassword, registerPassword2, onInputChange: onRegisterInputChange } = useForm( registerFormFields );
@@ -37,6 +39,14 @@ export const LoginPage = () => {
         event.preventDefault();
         console.log({registerName, registerEmail, registerPassword, registerPassword2})                 
     }
+
+    useEffect(() => {
+      if( errorMessage  !== undefined ){
+        Swal.fire('Error en la autenticacion', errorMessage, 'error');
+      }
+    
+    }, [errorMessage])
+    
 
     return (
         <div className="container login-container">
