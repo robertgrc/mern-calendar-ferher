98a1d47583fdf2877335d35bf9c0b262bad6e03d
robertgrc <robert.rodriguez.rgrc@gmail.com>
2022-09-14 13:39:01 -0400

    Limpiar la informacion del Calendario

--- src/hooks/useAuthStore.js
+++ src/hooks/useAuthStore.js
@@ -1,6 +1,12 @@
 import { useDispatch, useSelector } from "react-redux";
 import calendarApi from "../api/calendarApi";
-import { clearErrorMessage, onChecking, onLogin, onLogout } from "../store";
+import {
+  clearErrorMessage,
+  onChecking,
+  onLogin,
+  onLogout,
+  onLogoutCalendar,
+} from "../store";
 
 export const useAuthStore = () => {
   const { status, user, errorMessage } = useSelector((state) => state.auth);
@@ -64,6 +70,7 @@ export const useAuthStore = () => {
 
   const startLogout = () => {
     localStorage.clear();
+    dispatch(onLogoutCalendar());
     dispatch(onLogout());
   };
 
